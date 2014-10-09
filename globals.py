0f0974932398609f631b444e36647e5ec3ccc47f
Chris Blazick <chris@blazick.com>
2014-10-08 20:30:31 -0700

    added comments

--- globals.py
+++ globals.py
@@ -1,17 +1,27 @@
 import os
 
 def run():
+    """
+    execute the crux of the module
+    """
+
+    # open the file path in the package root
     fp = open(os.path.abspath(os.path.join(__file__, "../GLOBALS")))
+
+    # read each entry in the file
     for l in fp.readlines():
         l = l.strip()
+
+        # ignore commented sections
         nocomment = l.split("#", 1)[0]
         if nocomment == "":
             continue
 
+        # set the variables in the globals
         (var, val) = nocomment.split("=")
         var = var.strip()
         val = eval(val.strip())
 
         globals()[var] = val
 
-run()
+run() # automatically run on import. comment out to require manual activation
